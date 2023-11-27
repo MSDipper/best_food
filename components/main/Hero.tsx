8817aa1097daaa2a6cd26e1f6d0e11d2c7cb808c
MSDipper <github5248@gmail.com>
2023-11-27 20:46:40 +0300

    update

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,7 +3,7 @@ import HeroContent from '@/components/sub/HeroContent';
 
 const Hero = () => {
     return (
-        <div className='bg-[url(/bg.jpg)] relative flex flex-col h-full w-fullbg-no-repeat bg-cover bg-center min-h-screen'>
+        <div className='bg-[url(/bg.jpg)] relative flex flex-col h-full w-fullbg-no-repeat bg-cover bg-center min-h-screen z-0'>
             <HeroContent />
         </div>
     )
